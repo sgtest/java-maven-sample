c216c00ee1121bc5d428ac1d3e9c08db108a5723
Quinn Slack <qslack@qslack.com>
2014-12-20 06:10:06 -0800

    add code that uses the JAR dep

--- src/main/java/mypkg/Container.java
+++ src/main/java/mypkg/Container.java
@@ -1,5 +1,13 @@
 package mypkg;
 
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVRecord;
+
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.net.URL;
+
 public class Container {
 	public class Inner {
 		public Container F() {
@@ -9,5 +17,14 @@ public class Container {
 
 	public Inner F() {
 		return new Inner();
+	}
+
+	public void G() throws IOException {
+		Iterable<CSVRecord> records = CSVFormat.DEFAULT.parse(null);
+		for (CSVRecord record : records) {
+			String lastName = record.get("Last Name");
+			String firstName = record.get("First Name");
+			System.out.println(lastName + ", " + firstName);
+		}
 	}
 }
