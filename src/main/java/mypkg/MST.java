0d8004151455401bb7062eb75e6b2a0c3e2363f8
Alex Saveliev <lyolik@codeminders.com>
2016-06-01 15:08:25 +0300

    MST testcase
    - added member select tree (MST) testcase to make sure we are properly emitting spans when there are possible collisions between MST's expression and MST's identifier

---
+++ src/main/java/mypkg/MST.java
@@ -0,0 +1,15 @@
+package mypkg;
+
+import java.util.*;
+
+public class MST {
+
+    void foo() {
+        String s = "a".replaceAll("a", "b").replaceAll("x", "y");
+        List<String> list = Collections.<String>emptyList();
+        String replaceAll = "";
+        replaceAll.replaceAll("a","b");
+        replaceAll. replaceAll("c","d");
+        replaceAll .replaceAll("e","f");
+    }
+}
